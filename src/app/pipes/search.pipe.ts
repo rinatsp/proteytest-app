c833dced4cf0c66b114b8dba048a10bf0d13939f
Rinat <rinat95.95@yandex.ru>
2018-08-16 00:21:05 +0300

    Update pipe

--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -7,8 +7,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SearchPipe implements PipeTransform {
 
   transform(markers: any, value: string): any {
+    if(!markers) return [];
+    if(!value) return markers;
+
+    value = value.toLowerCase();
     return markers.filter(marker => {
-      return marker.name.includes(value);
+      return marker.name.toLowerCase().includes(value);
     });
   }
 
